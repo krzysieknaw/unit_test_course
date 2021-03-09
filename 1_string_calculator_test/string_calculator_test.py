a2dd7c7b656af3ef709308bf1255429b2d670401
Krzysztof Nawrocki <krzysieknaw@gmail.com>
2021-03-09 13:49:00 +0100

    final visual changes

--- 1_string_calculator_test/string_calculator_test.py
+++ 1_string_calculator_test/string_calculator_test.py
@@ -2,33 +2,32 @@ import unittest
 from string_calculator import string_calculator
 
 
-class test_string_calculator(unittest.TestCase):
+class TestStringCalculator(unittest.TestCase):
     def test_empty(self):
         actual = string_calculator("")
         expected = 0
         self.assertEqual(actual, expected)
 
     def test_single_nr(self):
-        for nr in ['0','1','12','-1','-12']:
+        for nr in ['0', '1', '12', '-1', '-12']:
             actual = string_calculator(nr)
             expected = int(nr)
             self.assertEqual(actual, expected)
 
     def test_multi_nrs(self):
-       actual = string_calculator('1,2,3')
-       expected = 6
-       self.assertEqual(actual, expected)
+        actual = string_calculator('1,2,3')
+        expected = 6
+        self.assertEqual(actual, expected)
 
-    def test_int_input(self): #  to fail
-       actual = string_calculator(1,2)
-       expected = 3
-       self.assertEqual(actual, expected)
+    def test_int_input(self):  # to fail
+        actual = string_calculator(1, 2)
+        expected = 3
+        self.assertEqual(actual, expected)
 
-    def test_non_numeric_input(self): #  to fail
-       actual = string_calculator('a,b,c')
-       expected = "what should I expect?"
-       self.assertEqual(actual, expected)
-
+    def test_non_numeric_input(self):  # to fail
+        actual = string_calculator('a,b,c')
+        expected = "what should I expect?"
+        self.assertEqual(actual, expected)
 
 
 if __name__ == '__main__':
