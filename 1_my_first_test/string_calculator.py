7e7e2eb1f278297e302aa8adb9f29835d38d78ff
Krzysztof Nawrocki <krzysieknaw@gmail.com>
2021-03-09 11:10:10 +0100

    Cleaned code - only string_calculator function left

--- 1_my_first_test/string_calculator.py
+++ 1_my_first_test/string_calculator.py
@@ -1,14 +1,3 @@
-# import unittest
-#
-#
-# clMyTestCase(unittest.TestCase):
-#     def test_something(self):
-#         self.assertEqual(True, False)
-#
-#
-# if __name__ == '__main__':
-#     unittest.main()
-
 def string_calculator():
     add = input("enter the number ")
     if add == "":
@@ -21,27 +10,8 @@ def string_calculator():
         return result
 
 
-print(string_calculator())
+def main():
+    print(string_calculator())
 
 
-# class StringCalculator
-# {
-#     int add(string input)
-#     {
-#         if (isEmpty(input))
-#         {
-#             return 0;
-#         }
-#         else
-#         {
-#             numbers = split(intput, ",")
-#             result = 0;
-#
-#             for (number in numbers)
-#             {
-#                 result += getIntFrom(number);
-#             }
-#             return result;
-#         }
-#     }
-# }
+main()
